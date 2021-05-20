ca46845baf3105675046ba046b35f2f38b637e9e
RotemKadosh <rotemkadosh27@gmail.com>
2021-05-20 17:25:06 +0300

    list sorted to show available representative first. now handling null and empty url for image resourc from data

--- app/src/main/java/com/example/support/networking/TeamMember.kt
+++ app/src/main/java/com/example/support/networking/TeamMember.kt
@@ -14,7 +14,7 @@ data class TeamMember(
         @Json(name = "available") val available: Boolean = false,
         @Json(name = "phone") val phone: String = "0548166800",
         @Json(name = "email") val email: String = "rotemkadosh27@gmail.com",
-        @Json(name = "image") val imgSrcUrl: String = "https://images.unsplash.com/photo-1528660493888-ab6f4761e036?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1800&amp;q=80" )
+        @Json(name = "image") var _imgSrcUrl: String ?)
         : Parcelable, Comparable<TeamMember> {
         override fun compareTo(other: TeamMember): Int {
             return if(this.available && !other.available){
@@ -22,6 +22,11 @@ data class TeamMember(
             } else{
                 1
             }
+        }
+    var imgSrcUrl:String = ""
+        get() {
+            if(_imgSrcUrl.isNullOrEmpty()) return "https://images.unsplash.com/photo-1528660493888-ab6f4761e036?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1800&amp;q=80"
+            return _imgSrcUrl as String
         }
 }
 
--- app/src/main/java/com/example/support/team/TeamFragment.kt
+++ app/src/main/java/com/example/support/team/TeamFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.*
 import android.view.inputmethod.EditorInfo
-import android.widget.Button
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -13,9 +12,9 @@ import com.example.support.R
 import com.example.support.databinding.TeamFragmentBinding
 
 
-class TeamFragment: Fragment() {
+class TeamFragment : Fragment() {
 
-    lateinit var binding : TeamFragmentBinding
+    lateinit var binding: TeamFragmentBinding
     private val viewModel: TeamViewModel by lazy {
         ViewModelProvider(this).get(TeamViewModel::class.java)
     }
@@ -24,11 +23,12 @@ class TeamFragment: Fragment() {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.d("lifecycle" , "fragment on createview")
+        Log.d("lifecycle", "fragment on createview")
         binding = TeamFragmentBinding.inflate(inflater)
 
         binding.lifecycleOwner = this
@@ -38,8 +38,6 @@ class TeamFragment: Fragment() {
         binding.teamList.adapter = MemberViewAdapter(MemberViewAdapter.OnClickListener {
             viewModel.displayMemberDetails(it)
         })
-
-
 
         viewModel.navigateToSelectedMember.observe(viewLifecycleOwner, {
             it?.let {
@@ -51,14 +49,25 @@ class TeamFragment: Fragment() {
         return binding.root
     }
 
-
-
-
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        Log.d("lifecycle" , "fragment on createoptionMenu")
+        Log.d("lifecycle", "fragment on createoptionMenu")
         inflater.inflate(R.menu.team_menu, menu)
         val itemSearch = menu.findItem(R.id.searc_action)
-        val searchView : SearchView = itemSearch.actionView as SearchView
+        val searchView: SearchView = itemSearch.actionView as SearchView
+        setQueryTextListener(searchView)
+        restoreSearchFocusIfExist(searchView)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    private fun restoreSearchFocusIfExist(searchView: SearchView) {
+        if (viewModel.searchText != null) {
+            searchView.requestFocus()
+            searchView.setQuery(viewModel.searchText, true)
+            searchView.isIconified = false
+        }
+    }
+
+    private fun setQueryTextListener(searchView: SearchView) {
         val options: Int = searchView.imeOptions
         searchView.imeOptions = options or EditorInfo.IME_FLAG_NO_EXTRACT_UI
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
@@ -66,50 +75,26 @@ class TeamFragment: Fragment() {
                 val adapter = (binding.teamList.adapter as MemberViewAdapter)
                 adapter.filter.filter(newText)
                 updateViewModelSearchText(newText)
-                updateResultsText()
                 return true
             }
+
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return false
             }
         })
-
-        if (viewModel.searchText != null){
-            searchView.requestFocus()
-            searchView.setQuery(viewModel.searchText, true )
-            searchView.isIconified = false
-        }
-
-        super.onCreateOptionsMenu(menu, inflater)
     }
 
-    private fun updateResultsText() {
-        Log.d("res", "update res called")
-        val adapter = (binding.teamList.adapter as MemberViewAdapter)
-        if(adapter.itemCount == 0)
-        {
-            Log.d("res", "update res itemCount == 0")
-            binding.resultsNotFound.visibility = View.VISIBLE
-            Log.d("res", "update res set visibile ")
-        }
-        else{
-            Log.d("res", "update res set gone ")
-            binding.resultsNotFound.visibility = View.GONE
-            Log.d("res", "item count is " + adapter.itemCount.toString())
-        }
-    }
-
-    private fun updateViewModelSearchText(query : String?){
+    private fun updateViewModelSearchText(query: String?) {
         Log.d("res", "update query called")
-        if(query?.isEmpty() == true){
+        if (query?.isEmpty() == true) {
             viewModel.searchText = null
-        }
-        else{
+        } else {
             viewModel.searchText = query
         }
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.refresh_action){
+        if (item.itemId == R.id.refresh_action) {
             viewModel.getTeamMembers()
         }
         return super.onOptionsItemSelected(item)
