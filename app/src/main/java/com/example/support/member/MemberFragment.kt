c39cdb0799ac613a0e1a28285668dd993113ea9e
RotemKadosh <rotemkadosh27@gmail.com>
2021-05-27 15:05:08 +0300

    lazy init of ViewModel

--- app/src/main/java/com/example/support/member/MemberFragment.kt
+++ app/src/main/java/com/example/support/member/MemberFragment.kt
@@ -1,34 +1,34 @@
-
 package com.example.support.member
 
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.support.R
 import com.example.support.RefreshViewModel
 import com.example.support.databinding.MemberFragmentBinding
 import com.example.support.networking.TeamMember
-import com.squareup.picasso.Picasso
+import com.example.support.team.TeamViewModel
 
 class MemberFragment : Fragment() {
     private lateinit var binding: MemberFragmentBinding
     private val refreshViewModel: RefreshViewModel by activityViewModels()
-    private lateinit var viewModel : MemberViewModel
+
+    private val viewModel by lazy {
+        ViewModelProvider(this, MemberViewModelFactory
+            (MemberFragmentArgs.fromBundle(requireArguments())
+            .selectedProperty, requireNotNull(activity).application)).get(MemberViewModel::class.java)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
 
-        val application = requireNotNull(activity).application
         binding = MemberFragmentBinding.inflate(inflater)
         binding.lifecycleOwner = this
-        val teamMember: TeamMember = MemberFragmentArgs.fromBundle(requireArguments()).selectedProperty
-        val viewModelFactory = MemberViewModelFactory(teamMember, application)
-        viewModel = ViewModelProvider(this, viewModelFactory).get(MemberViewModel::class.java)
         refreshViewModel.members.observe(viewLifecycleOwner, {
             viewModel.updateSelectedProperty(it)
         })
@@ -44,7 +44,7 @@ class MemberFragment : Fragment() {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.member_menu, menu)
-        val backItem  = menu.findItem(R.id.back_action)
+        val backItem = menu.findItem(R.id.back_action)
 
         backItem.setOnMenuItemClickListener {
             this.findNavController().navigateUp()
@@ -53,13 +53,12 @@ class MemberFragment : Fragment() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-         if (item.itemId == R.id.back_action){
+        if (item.itemId == R.id.back_action) {
             return false
-        }
-        else if(item.itemId == R.id.refresh_action){
+        } else if (item.itemId == R.id.refresh_action) {
             refreshViewModel.refreshData()
             return true
-         }
+        }
 
         return super.onOptionsItemSelected(item)
     }
